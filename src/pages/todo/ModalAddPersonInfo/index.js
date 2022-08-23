81771928a17e446f05ef909da1194e7b1955f8f3
Joey <3288432965@qq.com>
2022-08-23 19:16:18 +0800

    feat: 新增form元素

--- src/pages/todo/ModalAddPersonInfo/index.js
+++ src/pages/todo/ModalAddPersonInfo/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Form, Input, message } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, message, Select, Switch, Button, Radio } from 'antd';
+import { SketchPicker } from 'react-color'
 import { phoneNumberReg, emailReg } from '../../../consts/reg';
 import { getUserInfo, putUserInfo, postUserInfo } from '../../../api';
 import CommonModal from '../../../components/CommonModal';
@@ -8,8 +9,14 @@ import './index.css';
 
 const { TextArea } = Input;
 
+const radioMap = [
+  {key: '0', value: 0, label: 'input'},
+  {key: '1', value: 1, label: 'select'},
+]
+
 const ModalAddPersonInfo = (props) => {
   const [form] = Form.useForm();
+  const [currentRadio, setCurrentRadio] = useState(0);
   const { 
     visible, 
     close, 
@@ -20,13 +27,14 @@ const ModalAddPersonInfo = (props) => {
   } = props;
 
   function onCloseModal() {
-    close();
     onClose && onClose();
     form.setFieldsValue({
       userName: '',
       phoneNumber: '',
       email: '',
       personInfo: '',
+      picker: '',
+      switch: false,
     })
   }
 
@@ -74,6 +82,30 @@ const ModalAddPersonInfo = (props) => {
     });
   }
 
+  function onChangeRadio(e) {
+    setCurrentRadio(e.target.value);
+  }
+
+  const Picker = ({ value, onChange }) => {
+    const [showPicker, setShowPicker] = useState(false);
+
+    function onChangeColor(_color) {
+      onChange?.(_color.hex);
+    }
+
+    return (
+      <>
+        <Button style={{ backgroundColor: `${value}`}} onClick={() => setShowPicker(true)}> 点我更换颜色 </Button>
+        {showPicker ? (
+          <div className="pick-background-color__popover">
+            <div className="pick-background-color__cover" onClick={() => setShowPicker(false)} />
+            <SketchPicker color={value || ''} onChange={onChangeColor} />
+          </div>
+        ) : null}
+      </>
+    )
+  }
+
   return (
     <CommonModal
       title={type === 'add' ? '请填写您的个人信息' : '请修改您的个人信息'}
@@ -82,7 +114,7 @@ const ModalAddPersonInfo = (props) => {
       cancelText='取消'
       visible={visible}
       onOk={onClickOk}
-      onCancel={onCloseModal}
+      onCancel={close}
       onOpen={onOpenModal}
       onClose={onCloseModal}
     >
@@ -117,6 +149,36 @@ const ModalAddPersonInfo = (props) => {
         <Form.Item name='personInfo' label='个人介绍' >
           <TextArea rows={4} placeholder='请输入' />
         </Form.Item>
+        <Form.Item name='switch' valuePropName="checked" label='开关' >
+          <Switch />
+        </Form.Item>
+        <Form.Item name='picker' label='颜色选择' >
+          <Picker />
+        </Form.Item>
+        <Form.Item>
+          <Radio.Group
+            onChange={onChangeRadio}
+            value={currentRadio}  
+          >
+            {radioMap.map(item => {
+              return (
+                <Radio value={item.value} key={item.key}>{item.label}</Radio>
+              )
+            })}
+          </Radio.Group>
+        </Form.Item>
+        {currentRadio === 0 && (
+          <Form.Item name='input'>
+            <Input placeholder='请输入'/>
+          </Form.Item>
+        )}
+        {currentRadio === 1 && (
+          <Form.Item name='select'>
+            <Select>
+              <Select.Option value="1">111</Select.Option>
+            </Select>
+          </Form.Item>
+        )}
       </Form>
     </CommonModal>
   );
